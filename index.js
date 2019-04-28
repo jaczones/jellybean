9ce74cf33abf474b2ef074d8d42e03c3a5d7a37c
jaczones <zjones@twitter.com>
2019-04-28 10:36:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -45,30 +45,27 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
-            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
-            // Otherwise just echo back to them what they sent us.
-
-            // but first, let's make sure the token matches!
+        case "/jellybean": 
             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
 
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+                    "I tell you what your preferred jellybean color says about you. " +
+                    "Try typing `/jellybean red` to see.");
                 return;
             }
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
+            if (message.text === "red") {
+                slashCommand.replyPrivate(message, "1", function() {
+                    slashCommand.replyPrivateDelayed(message, "2").then(slashCommand.replyPrivateDelayed(message, "3"));
+                });
 
             break;
         default:
-            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+            slashCommand.replyPrivate(message, "I'm afraid I don't know how to " + message.command + " yet.");
 
     }
 
